d55b673d1f135fb8da7a01fba0b99d6097917edc
Daniel Harris <dwh261@gmail.com>
2024-10-18 17:48:58 +0000

    feat(models:add Tag model and ManyToManyField to Post model

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -15,7 +22,7 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
-
+    tags = models.ManyToManyField(Tag, related_name="posts")
 
     class Meta:
         ordering = ['-created_on']
@@ -37,4 +44,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post.title}"
-
