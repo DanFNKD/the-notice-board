4b69436d6e988d21333a1a5499803e6446aee95e
Daniel Harris <dwh261@gmail.com>
2024-10-24 09:58:54 +0000

    feat(summernote): integrate and confirgure Summernote editor

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Post, Comment, Tag, Vote
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'author', 'created_on')
+    search_fields = ('title',)
+    list_filter = ('status',)
+    ordering = ['status', 'title']
+    list_editable = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Tag)
 admin.site.register(Vote)
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from .import views
 from django.urls import path
+import django_summernote
 
 urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
